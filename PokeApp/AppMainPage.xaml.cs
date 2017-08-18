5385e46da3fbc6d47c792d2c14b04137e1c47b0d
Adam Cardenas <cevaris@gmail.com>
2017-08-18 08:10:06 -0600

    initial

---
+++ PokeApp/AppMainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace PokeApp
+{
+    public partial class AppMainPage : ContentPage
+    {
+        public AppMainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

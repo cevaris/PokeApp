3d7e090e34b7249d86e31d97abbb6e59b675eb67
Adam Cardenas <cevaris@gmail.com>
2017-08-18 08:43:31 -0600

    working http query and deserialize to object

--- PokeApp/AppMainPage.xaml.cs
+++ PokeApp/AppMainPage.xaml.cs
@@ -1,15 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Net.Http;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace PokeApp
 {
+
+    public class Pokemon
+    {
+        public Pokemon() { }
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+
     public partial class AppMainPage : ContentPage
     {
+
+        HttpClient client;
+        Pokemon PokemonItem;
+
         public AppMainPage()
         {
             InitializeComponent();
+
+            PokemonItem = new Pokemon();
+            client = new HttpClient();
+            fetchDataz();
+        }
+
+        private async void fetchDataz()
+        {
+            //client.DefaultRequestHeaders.Add("Content-Type","application/json");
+
+            const string url = "https://pokeapi.co/api/v2/pokemon/2/";
+
+            var uri = new Uri(url);
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine($"content={content}");
+                PokemonItem = Newtonsoft.Json.JsonConvert.DeserializeObject<Pokemon>(content);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine($"response={response}");
+            }
+
         }
     }
 }
---
+++ PokeApp/README.md
@@ -0,0 +1,8 @@
+﻿
+Pokemon API
+- http://pokeapi.co/docsv2/#pokemon-section
+
+JSON.net
+- https://www.newtonsoft.com/json
+
+

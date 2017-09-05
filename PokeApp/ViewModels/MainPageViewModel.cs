a8c2359dc1d10629f9bf1a1c266a7ba86b0adbf1
Adam Cardenas <cevaris@gmail.com>
2017-09-05 07:05:13 -0600

    save

--- PokeApp/ViewModels/MainPageViewModel.cs
+++ PokeApp/ViewModels/MainPageViewModel.cs
@@ -5,7 +5,7 @@ namespace PokeApp
     {
         public static MainPageViewModel Preview = new MainPageViewModel()
         {
-            PokedexUrl = "https://s3-us-west-2.amazonaws.com/pokeapp.assets/app/images/dex2.png",
+            PokedexUrl = "http://icons.iconarchive.com/icons/thiago-silva/palm/256/Photos-icon.png",
             AboutUrl = "https://s3-us-west-2.amazonaws.com/pokeapp.assets/app/images/jenny.png"
         };
 
--- PokeApp/Views/MainPage.xaml.cs
+++ PokeApp/Views/MainPage.xaml.cs
@@ -13,7 +13,12 @@ namespace PokeApp
             BindingContext = MainPageViewModel.Preview;
             InitializeComponent();
 
-            PokedexImage.Source = ImageSource.FromUri(new Uri(MainPageViewModel.Preview.PokedexUrl));
+            //PokedexImage.Source = ImageSource.FromUri(new Uri(MainPageViewModel.Preview.PokedexUrl));
+            //PokedexImage.Source = ImageSource.FromUri(new Uri("http://icons.iconarchive.com/icons/thiago-silva/palm/256/Photos-icon.png"));
+            PokedexImage.Source = new UriImageSource { 
+                CachingEnabled = false, 
+                Uri = new Uri("http://icons.iconarchive.com/icons/thiago-silva/palm/256/Photos-icon.png") 
+            };
 
         }
     }

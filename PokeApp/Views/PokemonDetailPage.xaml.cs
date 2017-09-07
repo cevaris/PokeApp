7fb212a2117aa13f41c17f3ad0bff85ab2c00443
Adam Cardenas <cevaris@gmail.com>
2017-09-07 08:52:54 -0600

    working responsive detail view

--- PokeApp/Views/PokemonDetailPage.xaml.cs
+++ PokeApp/Views/PokemonDetailPage.xaml.cs
@@ -1,12 +1,30 @@
-﻿using Xamarin.Forms;
+﻿using PokeApp.Utils;
+using Xamarin.Forms;
 
 namespace PokeApp
 {
     public partial class PokemonDetailPage : ContentPage
     {
+        private ILogger logger = new ConsoleLogger(nameof(MainPage));
+
         public PokemonDetailPage()
         {
             InitializeComponent();
+        }
+
+        protected override void OnSizeAllocated(double width, double height)
+        {
+            base.OnSizeAllocated(width, height);
+            if (width > height)
+            {
+                DetailSection.Orientation = StackOrientation.Horizontal;
+                logger.Info("setting orientation to Horizontal");
+            }
+            else
+            {
+                DetailSection.Orientation = StackOrientation.Vertical;
+                logger.Info("setting orientation to Vertical");
+            }
         }
     }
 }

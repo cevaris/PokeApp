aa5237cefb4b1949c565d0db5eb7f32859d648de
Adam Cardenas <cevaris@gmail.com>
2017-09-12 10:31:49 -0600

    test

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
-using Google.MobileAds;
 using UIKit;
 
 namespace PokeApp.iOS
@@ -11,7 +10,6 @@ namespace PokeApp.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
-
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
@@ -19,11 +17,6 @@ namespace PokeApp.iOS
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
-        }
-
-        public override bool WillFinishLaunching(UIApplication uiApplication, NSDictionary launchOptions) {
-            MobileAds.Configure(Secrets.AppId);
-            return base.WillFinishLaunching(uiApplication, launchOptions);
         }
     }
 }
